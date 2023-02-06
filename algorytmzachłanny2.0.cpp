6d86d38224428b8956b13c23ff043457447dc9d9
jakubekgranie <124626746+jakubekgranie@users.noreply.github.com>
2023-02-06 22:41:32 +0100

    Update algorytmzachłanny2.0.cpp

--- algorytmzachłanny2.0.cpp
+++ algorytmzachłanny2.0.cpp
@@ -1,17 +1,16 @@
-﻿//n = nominals[i];
+//n = nominals[i];
 //autor: Jakub N;
 //ALGORYTM ZACHŁANNY
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
 int main()
 {
     int nominals[15] = { 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 }; // x = nominał * 100;
     double numer;
     cin >> numer;
     numer = numer * 100;  //usuwanie przecinka;
-    int numer2 = (int)numer; //konwersja z typu double na int;
+    int numer2 = std::round(numer); //konwersja z typu double na int;
     while (numer2 != 0)
     {
         for (int i = 0; i < 15; i++) //właściwy algorytm;
